965eaf93d715e5cb516947947e954fd3af3d9fb0
shbin <shbin05@naver.com>
2022-11-06 08:09:44 +0900

    Update AuthComponent.js

--- component/AuthComponent.js
+++ component/AuthComponent.js
@@ -3,8 +3,10 @@ import {
     View,
     Text,
     Image,
-    TouchableOpacity
+    TouchableOpacity,
+    Dimensions
 } from 'react-native';
+import { colors } from '../colors';
 
 const data = {
     title: '오늘도 내가 먼저 인사 ^^',
@@ -19,12 +21,22 @@ const data = {
     commentCount: 2,
 }
 
+const Width = Dimensions.get('screen').width;
+const Height = Dimensions.get('screen').height;
+
+const Layout = { Width: Width, Height: Height };
+const FontScale = 1.3;
+
 export default function AuthComponent(data) {
   	return (
 		<TouchableOpacity
 			style={{
 				height: 100,
-                backgroundColor: "lightgray"
+				width: Layout.Width,
+                backgroundColor: "#F7F7FB",
+                justifyContent: "center",
+                borderBottomColor: "black",
+                borderBottomWidth: 1
 			}}
 		>
 			<View
@@ -37,13 +49,26 @@ export default function AuthComponent(data) {
                 <Image 
                     source = {{uri: data.image}}
                     style={{
-                        width: 40,
-                        height: 40
+                        width: 70,
+                        height: 70,
+                        marginTop: 10,
+                        marginLeft: 15,
+                        borderRadius: 15
                     }}
                 />
-                <View>
-                    <Text>{data.title}</Text>
+                <View style={{marginLeft: 15, width: "70%"}}>
+                    <Text style={{fontWeight: "900", marginTop: 5}}>{data.title}</Text>
                     <Text>{data.campaign}</Text>
+                    <View style={{display: "flex", flexDirection: "row", justifyContent: "space-between", marginTop: 20}}>
+                        <Text style={{fontSize: 12}}>닉네임: {data.writer}</Text>
+                        <View>
+                            <Text style={{fontSize: 12}}>{data.date}</Text>
+                            <View style={{display: "flex", flexDirection: "row"}}>
+                                <Text style={{fontSize: 12, marginRight: 5}}>조회 {data.viewCount}</Text>
+                                <Text style={{fontSize: 12}}>댓글 {data.commentCount}</Text>
+                            </View>
+                        </View>
+                    </View>
                 </View>
             </View>
 		</TouchableOpacity>
