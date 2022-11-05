b73560e68e50ec151759e173cb1566788b7529ae
ParkSeongWan <dreamnawas@naver.com>
2022-11-05 21:35:30 +0900

    login 기능 구현

--- src/users/createAccount/createAccount.resolvers.js
+++ src/users/createAccount/createAccount.resolvers.js
@@ -6,17 +6,47 @@ export default {
         createAccount: async (
             _, { userName, gender, userId, userPassword }
         ) => {
-            await client.user.create({
-                data: {
-                    userName,
-                    gender,
-                    userId,
-                    userPassword,
+            try {
+                const existinguserName = await client.user.findFirst({
+                    where : {
+                        userName
+                    },
+                });
+                const existinguserId = await client.user.findFirst({
+                    where : {
+                        userId,
+                    },
+                });
+                if(existinguserId) {
+                    return {
+                        ok : false,
+                        error : "This userId is already used",
+                    }
                 }
-            });
-            return {
-                ok: true,
-            };
+                else if(existinguserName){
+                    return {
+                        ok: false,
+                        error : "This userName is already used",
+                    }
+                }
+                await client.user.create({
+                    data: {
+                        userName,
+                        gender,
+                        userId,
+                        userPassword,
+                    }
+                });
+                return {
+                    ok: true,
+                };
+            }
+            catch (e) {
+                return {
+                    ok: false,
+                    error: e.message,
+                }
+            }
         }
     }
 }
